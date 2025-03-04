647cf8ed132f0dc76c68523bcd323983bc187a1f
franciiscocg <cisquitofrancisco@gmail.com>
2025-03-04 16:40:42 +0100

    entorno djanjo react

--- App/main/forms.py
+++
@@ -1,8 +0,0 @@
-#encoding:utf-8
-from django import forms
-   
-class UsuarioBusquedaForm(forms.Form):
-    idUsuario = forms.CharField(label="Id de Usuario", widget=forms.TextInput, required=True)
-    
-class PeliculaBusquedaForm(forms.Form):
-    idPelicula = forms.CharField(label="Id de Pelicula", widget=forms.TextInput, required=True)
