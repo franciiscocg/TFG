cc0cd2cc6f6e50e0ec8d009de2e8508b20d83097
franciiscocg <cisquitofrancisco@gmail.com>
2025-03-20 20:02:02 +0100

    subida de archivos con proteccion

--- myproject/archivos/models.py
+++ myproject/archivos/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+import os
+
+def user_directory_path(instance, filename):
+    safe_username = ''.join(c for c in instance.user.username if c.isalnum())
+    return f'uploads/{safe_username}/{filename}'
 
 class UploadedFile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    file = models.FileField(upload_to='uploads/') 
+    file = models.FileField(upload_to=user_directory_path)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
